b7455cf75e86ea2a057bf17329790bc0e2c3b5a7
kimlan nguyen <kimlan@>
2017-04-26 14:11:13 -0700

    add buttons for subtract, multiply, divide

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,51 @@ var divide = function(number1, number2){
 	return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number: "));
+$(document).ready(function() {
+	/*get add button to do addition only*/
+	$("#button1").click(function(){
+		$("form#calculator").submit(function(event){
+			var number1 = parseFloat($("#number1").val());
+			var number2 = parseFloat($("#number2").val());
+			var result = add(number1, number2);
+			$("#output1").text(result);
 
-alert(subtract(number1, number2));
+			event.preventDefault();
+		});
+	});
+	/*get subtract button to do subtraction only*/
+	$("#button2").click(function(){
+		$("form#calculator").submit(function(event){
+			var number1 = parseFloat($("#number1").val());
+			var number2 = parseFloat($("#number2").val());
+			var result = subtract(number1, number2);
+			$("#output1").text(result);
+
+			event.preventDefault();
+		});
+	});
+
+	$("#button3").click(function(){
+		$("form#calculator").submit(function(event){
+			var number1 = parseFloat($("#number1").val());
+			var number2 = parseFloat($("#number2").val());
+			var result = multiply(number1, number2);
+			$("#output1").text(result);
+
+			event.preventDefault();
+		});
+	});
+
+	$("#button4").click(function(){
+		$("form#calculator").submit(function(event){
+			var number1 = parseFloat($("#number1").val());
+			var number2 = parseFloat($("#number2").val());
+			var result = divide(number1, number2);
+			$("#output1").text(result);
+
+			event.preventDefault();
+		});
+	});
+
+
+});
