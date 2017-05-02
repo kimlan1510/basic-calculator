2b1f838b57e737799cccd052b2bbe9f642f7795c
Kimlan Nguyen <kimlan1510@yahoo.com>
2017-05-02 00:02:21 -0700

    change how calculator work. Make actual calculator ui

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,32 @@
-var add = function(number1, number2){
-	return number1 + number2;
-};
-
-var subtract = function(number1, number2){
-	return number1 - number2;
-};
-
-var multiply = function(number1, number2){
-	return number1 * number2;
-};
-
-var divide = function(number1, number2){
-	return number1 / number2;
-};
-
-$(document).ready(function() {
-	/*get add button to do addition only*/
-	$("#button1").click(function(){
-		$("form#calculator").submit(function(event){
-			var number1 = parseFloat($("#number1").val());
-			var number2 = parseFloat($("#number2").val());
-			var result = add(number1, number2);
-			$("#output1").text(result);
-
-			event.preventDefault();
-		});
-	});
-	/*get subtract button to do subtraction only*/
-	$("#button2").click(function(){
-		$("form#calculator").submit(function(event){
-			var number1 = parseFloat($("#number1").val());
-			var number2 = parseFloat($("#number2").val());
-			var result = subtract(number1, number2);
-			$("#output1").text(result);
-
-			event.preventDefault();
-		});
-	});
-
-	$("#button3").click(function(){
-		$("form#calculator").submit(function(event){
-			var number1 = parseFloat($("#number1").val());
-			var number2 = parseFloat($("#number2").val());
-			var result = multiply(number1, number2);
-			$("#output1").text(result);
-
-			event.preventDefault();
-		});
-	});
-
-	$("#button4").click(function(){
-		$("form#calculator").submit(function(event){
-			var number1 = parseFloat($("#number1").val());
-			var number2 = parseFloat($("#number2").val());
-			var result = divide(number1, number2);
-			$("#output1").text(result);
-
-			event.preventDefault();
-		});
-	});
-
+function add(number1, number2){
+  return number2 + number1;
+}
+function substract(number1, number2){
+  return number2-number1;
+}
+function multiply(number1, number2){
+  return number2*number1;
+}
+function divide(number1, number2){
+  return number2/number1;
+}
+var array_number = [];
+var display_result = 0;
+$(document).ready(function(){
+  // 
+  $("td.number").click(function(){
+    array_number.push($(this).text());
+    number = array_number.join("");
+    $("li.result").text(number);
+  });
+  $("td.operator").click(function(){
+    if (this === $("td#plus")) {
+      number = number1
+      display_result = add(number1, number2);
+    }
+    ("td#equal").click(function(){
+      $("li.result").text(display_result);
+    });
+  });
 
 });
